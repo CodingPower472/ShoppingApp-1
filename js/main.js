6aea95cffba83eba3b707389de84520f17a0ed6f
Luke <tech.master.champ@gmail.com>
2015-08-03 00:25:24 +0000

    Refractored with Angular

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-	$('#add').click(function(){
-		$('#list').append('<li>' + $('#item').val() + '</li>');
-		$('#item').val('');
-	});
-});
+function MainController () {
+  var vm = this;
+  vm.list = [];
+  vm.add = function () {
+    vm.list.push(vm.item);
+    vm.item = '';
+  };
+  vm.remove = function (index) {
+    vm.list.splice(index, 1);
+  };
+}
+
+angular.module('shopping', [])
+
+.controller('MainController', MainController);
